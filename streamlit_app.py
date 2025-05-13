c6b23591cb057d5570ffbc63f7e5b2b26c7ec259
kelompok3pli <hakimluki31@gmail.com>
2025-05-13 15:37:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,3 +105,21 @@ elif menu == "ℹ️ Tentang":
          Mr adalah jumlah massa atom dari seluruh atom dalam suatu molekul. Digunakan untuk
          menghitung massa molar senyawa dalam satuan g/mol saat dikalikan dengan 1 mol.
     """)
+
+# CSS untuk mengubah latar belakang menjadi abu-abu
+page_bg = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-color: #f0f0f0;  /* Abu-abu terang */
+}
+
+[data-testid="stHeader"] {
+    background-color: rgba(0,0,0,0); /* Transparan */
+}
+
+[data-testid="stSidebar"] {
+    background-color: rgba(255, 255, 255, 0.9); /* Sidebar putih semi-transparan */
+}
+</style>
+"""
+st.markdown(page_bg, unsafe_allow_html=True)
